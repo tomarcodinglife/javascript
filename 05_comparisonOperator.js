a6ba619e32670ca92922537f478535df730ce069
Sujit Tomar <tomarcodinglife@gmail.com>
2024-09-08 07:19:57 +0530

    Add files via upload

---
+++ 05_comparisonOperator.js
@@ -0,0 +1,15 @@
+let memoryFirst = 2;
+let memorySecond = 5;
+let memoryThird = 8;
+let memoryFourth = 18;
+let memoryFifth = 8;
+
+console.log (memoryFirst == memorySecond)
+console.log (memoryFirst > memorySecond)
+console.log (memoryFirst < memorySecond)
+console.log (memoryFifth <= memoryFifth )
+
+
+console.log("------------------------Here----------------------")
+console.log ("5" == 5)
+console.log ("5" === 5)
