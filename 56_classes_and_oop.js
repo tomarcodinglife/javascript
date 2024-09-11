53d974470785f444b3ed141a9bf9ce4e5367e6c7
tomarcodinglife <tomarcodinglife@gmail.com>
2024-09-11 07:15:03 +0530

    updated

---
+++ 56_classes_and_oop.js
@@ -0,0 +1,32 @@
+/*
+
+# JavaScript and classes
+# OOP
+
+#Object
+- Collection of Properties and method
+- toLowerCase
+
+
+#Why use oop?
+
+#Part of oop?
+-Object Literal
+
+
+-Constructor function
+-Prototype
+-Classes
+-Instance (new, this)
+
+
+4pillars :- 
+
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
+
+*/
+
+
