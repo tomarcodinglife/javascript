da6a6c8de77088308eb64f0288b10995eea4a444
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-01 09:56:43 +0530

    updated

--- 25_Revision/03_operator/operator.js
+++ 25_Revision/03_operator/operator.js
@@ -78,6 +78,119 @@ console.log("************************Comparison Operators*******************")
 let m = 5
 let n = 2
 let o = 6
-let p = 2
+let p = "2"
 
 
+console.table([
+
+    {Operator:"==", Name: "Equal to", Example:"n==p", return:n==p},
+    {Operator:"===", Name: "Strict equal to", Example:"n===p", return:n===p},
+    {Operator:"!=", Name: "Not equal to", Example:"m!=n", return:m!=n},
+    {Operator:"!==", Name: "Strict not equal to", Example:"m!==n", return:m!==n},
+    {Operator:">", Name: "Greater than", Example:"m>n", return:m>n},
+    {Operator:"<", Name: "Less than", Example:"m<n", return:m<n},
+    {Operator:">=", Name: "Greater than or equal to", Example:"m>=n", return:m>=n},
+    {Operator:"<=", Name: "Less than or equal to", Example:"m<=n", return:m<=n},
+
+])
+
+/*
+
+┌─────────┬──────────┬────────────────────────────┬─────────┬────────┐
+│ (index) │ Operator │ Name                       │ Example │ return │
+├─────────┼──────────┼────────────────────────────┼─────────┼────────┤
+│ 0       │ '=='     │ 'Equal to'                 │ 'n==p'  │ true   │
+│ 1       │ '==='    │ 'Strict equal to'          │ 'n===p' │ false  │
+│ 2       │ '!='     │ 'Not equal to'             │ 'm!=n'  │ true   │
+│ 3       │ '!=='    │ 'Strict not equal to'      │ 'm!==n' │ true   │
+│ 4       │ '>'      │ 'Greater than'             │ 'm>n'   │ true   │
+│ 5       │ '<'      │ 'Less than'                │ 'm<n'   │ false  │
+│ 6       │ '>='     │ 'Greater than or equal to' │ 'm>=n'  │ true   │
+│ 7       │ '<='     │ 'Less than or equal to'    │ 'm<=n'  │ false  │
+└─────────┴──────────┴────────────────────────────┴─────────┴────────┘
+
+*/
+
+// Logical Operators
+console.log("************************Logical Operators*******************")
+
+let BoolA = true
+let BoolB = false
+let BoolC = true
+
+console.table([
+
+    {operator: "AND (&&)", Example:"BoolA && BoolB", Value:BoolA && BoolB},
+    {operator: "OR (||)", Example:"BoolA || BoolB", Value:BoolA || BoolB},
+    {operator: "NOT (!)", Example:" (!BoolB)", Value:(!BoolB)},
+
+])
+
+/*
+
+┌─────────┬────────────┬──────────────────┬───────┐
+│ (index) │ operator   │ Example          │ Value │
+├─────────┼────────────┼──────────────────┼───────┤
+│ 0       │ 'AND (&&)' │ 'BoolA && BoolB' │ false │
+│ 1       │ 'OR (||)'  │ 'BoolA || BoolB' │ true  │
+│ 2       │ 'NOT (!)'  │ ' (!BoolB)'      │ true  │
+└─────────┴────────────┴──────────────────┴───────┘
+
+*/
+
+// Ternary Operator (? :)
+console.log("************************Ternary Operator*******************")
+
+let age = 22
+let AgeCheck = (age >= 18) ? "Adult" : "Minor";
+console.log(AgeCheck) // Adult
+
+
+// Type Operators
+console.log("************************Type Operators*******************")
+
+let fname = "Sujit"
+console.log(typeof(fname)) // 
+
+class student {
+    constructor(studentName, studentAge){
+        this.studentName = studentName;
+        this.studentAge = studentAge;
+    }
+};
+
+class employee {
+    constructor(employeeName, employeeAge){
+        this.employeeName = employeeName;
+        this.employeeAge = employeeAge;
+    }
+}
+
+StudentFirst = new student("Sujit", 22);
+StudentSecond = new employee("Amit", 25);
+
+console.log(StudentFirst instanceof student) // true
+console.log(StudentSecond instanceof student) // false
+
+// Spread (...) and Rest (...) Operators
+
+let arrayFirst = [5,6,8,2,47,9,5,]
+let arraySecond = [...arrayFirst, 9, 5, 8, 6, 5, 6]
+
+console.log(arraySecond) // [5, 6, 8, 2, 47, 9, 5, 9, 5, 8,  6, 5, 6]
+
+
+function sum(...num){
+    return num.reduce((a, b) => a+b, 0)
+} 
+
+console.log(sum(1, 2, 3, 4, 5, 6)) // 21
+
+// Nullish Coalescing Operator (??)
+
+let user = null;
+let defaultName = user?? "Guest";
+let result ="Pass"
+let passStatus = result?? "Fail"
+console.log(defaultName) // Guest
+console.log(passStatus) // Pass
---
+++ 25_Revision/04_string/string.js
@@ -0,0 +1,19 @@
+let userName = "Sujit Tomar"
+let userFirstName = "Amit"
+let userSecondName = "Singh"
+let userLastName = "Rajput"
+let userFirst = "      Rohit      "
+let userAge = 23
+
+console.log(`my name is ${userName} and age is ${userAge}`) // my name is Sujit Tomar and age is 23
+
+console.log(userName.toLowerCase())// sujit tomar
+console.log(userName.toUpperCase())//  SUJIT TOMAR
+console.log(userFirstName.charAt(2)) // i
+console.log(userFirstName.indexOf("it")) // 2
+console.log(userName.substring(0, 5))// Sujit
+console.log(userName.slice(0, 5)) // Sujit
+console.log(userFirst.trim()) // Rohit (return without white space )
+console.log(userFirst.trim(2, 9)) // 
+
+
