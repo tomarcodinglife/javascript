ca43c0252f93a1b2b654a38c83881e0a47c6ce75
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-02 20:55:43 +0530

    updated

---
+++ 25_Revision/07_array/array.js
@@ -0,0 +1,31 @@
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray[2]) // 3
+
+let myHero = ["Shaktiman", "Naagraj", "Spider"]
+console.log(myHero[2]) // Spider 
+
+let arr = [1, 2, 3]
+arr.push(4)
+console.log(arr) // [ 1, 2, 3, 4 ]
+arr.pop() 
+console.log(arr) // [ 1, 2, 3 ]
+
+let arrayA = [5, 6, 8, 8]
+arrayA.unshift(2)
+console.log(arrayA) // [ 2, 5, 6, 8, 8 ]
+arrayA.shift()
+console.log(arrayA) // [ 5, 6, 8, 8 ]
+arrayA.shift()
+console.log(arrayA) // [ 6, 8, 8 ]
+
+let newArry = [5, 8, 9, 60]
+console.log(newArry.includes(9)) // true
+
+let arrayB = [8, 5, 9, 5, 6, 2]
+console.log(arrayB) //    [ 8, 5, 9, 5, 6, 2 ]
+console.log(arrayB.join())  //  8,5,9,5,6,2
+
+let arrayc = ["Sujit", "Amit", "Rohit", "Sumit", "Shekhar", "Sohail", "Samir", "Ranjan", "Niraj"]
+console.log(arrayc) // 
+
+
