95d466839fb3483a2a6f2c75c194d0e326ca29b1
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-08 20:49:59 +0530

    updated

---
+++ 25_Revision/12_loop/loop.js
@@ -0,0 +1,39 @@
+// for loop
+let x = 5
+
+// for (let i = 0; i<=x; i++) {
+//     let row = '-'
+
+//     for (let j = 0; j<=i; j++) {
+//         let colunm = '*'
+//         row += colunm
+//     }
+
+//     console.log(row)    
+// };
+
+let y = 5
+
+for (let i=y; i>=1; i--) {
+    let row = ''
+    for (let j=1; j<=i; j++) {
+        let colunm = '*'
+        row += colunm
+    }
+    console.log(row)    
+}
+
+
+
+
+// while loop
+
+// while (i=0 > x) {
+//     console.log("While Loop")
+// }
+
+// do while loop
+
+// do {
+//     console.log("do while")
+// } while (i=0 > x);
