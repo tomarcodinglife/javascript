a3551ba99b4b4c8523f383420f37523557dea9ed
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-10 07:43:51 +0530

    updated

--- 25_Revision/12_loop/loop.js
+++ 25_Revision/12_loop/loop.js
@@ -238,3 +238,36 @@ let filterArray = array6.filter(function (element) {
 console.log(filterArray) // [6, 7, 8, 9, 10]
 console.log(array6.filter((element) => element > 5)) // [6, 7, 8, 9, 10]  
 
+// filter with object
+
+let students = [
+    { name: "Sujit", age: 25, city: "Delhi" },
+    { name: "Rahul", age: 30, city: "Mumbai" },
+    { name: "Amit", age: 35, city: "Bangalore" },
+    { name: "Ravi", age: 40, city: "Chennai" }
+]
+
+let filterStudents = students.filter(function (student) {
+    return student.age > 30
+})
+console.log(filterStudents) 
+
+/* 
+
+[
+  { name: 'Amit', age: 35, city: 'Bangalore' },
+  { name: 'Ravi', age: 40, city: 'Chennai' }
+]
+
+*/
+
+let filterStudents1 = students.filter((student) => student.age > 30 && student.city === "Bangalore")
+console.log(filterStudents1) // [ { name: 'Amit', age: 35, city: 'Bangalore' } ]    
+
+
+let newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let myNewArray = newArray.map(function (element) {
+    return element + 10
+})
+
+console.log(myNewArray) // [11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
