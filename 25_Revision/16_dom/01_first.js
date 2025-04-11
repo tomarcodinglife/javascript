af318c9cde001fbf19e85d75ab1c8aeb21bfed93
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-11 09:22:19 +0530

    updated

---
+++ 25_Revision/16_dom/01_first.js
@@ -0,0 +1,65 @@
+let title = document.getElementsByClassName("title");
+let container = document.getElementsByClassName("container");
+
+// selectors
+// document.getElementById("id");
+// document.getElementsByClassName("class");
+// document.getElementsByTagName("tag");
+// document.querySelector("selector");
+// document.querySelectorAll("selector");
+// document.getElementsByName("name");
+// document.getElementsByClassName("class")[0].style.color = "red";
+// document.getElementsByTagName("tag")[0].style.color = "red";
+// document.querySelector("selector").style.color = "red";
+// document.querySelectorAll("selector")[0].style.color = "red";
+// document.getElementsByName("name")[0].style.color = "red";
+// document.getElementsByClassName("title")[0].style.color = "red";
+
+
+title[2].textContent = "Hello World!";
+title[2].style.color = "red";
+// title[2].style.fontSize = "50px";
+// title[2].style.fontWeight = "bold";
+// title[2].style.textAlign = "center";
+// title[2].style.textDecoration = "underline";
+// title[2].style.textTransform = "uppercase";
+// title[2].style.backgroundColor = "black";
+// title[2].style.padding = "10px";
+// title[2].style.borderRadius = "10px";
+// title[2].style.boxShadow = "0 0 10px red";
+// title[2].style.margin = "10px";
+// title[2].style.width = "100%";
+// title[2].style.height = "100px";
+// title[2].style.display = "flex";
+// title[2].style.alignItems = "center";
+// title[2].style.justifyContent = "center";
+// title[2].style.fontFamily = "Arial, sans-serif";
+// title[2].style.fontStyle = "italic";
+// title[2].style.lineHeight = "100px";
+// title[2].style.letterSpacing = "5px";
+// title[2].style.textShadow = "2px 2px 5px black";
+title[2].style.transition = "all 0.5s ease-in-out";
+// title[2].style.cursor = "pointer";
+// title[2].style.opacity = "0.8";
+// title[2].style.filter = "blur(0px)";
+title[2].style.transform = "scale(1.1)";
+// title[2].style.zIndex = "1";
+// title[2].style.position = "relative";
+// title[2].style.top = "0px";
+// title[2].style.left = "0px";
+// title[2].style.right = "0px";
+// title[2].style.bottom = "0px";
+// title[2].style.border = "2px solid red";
+// title[2].style.boxSizing = "border-box";
+// title[2].style.overflow = "hidden";
+// title[2].style.whiteSpace = "nowrap";
+// title[2].style.textOverflow = "ellipsis";
+// title[2].style.visibility = "visible";
+// title[2].style.opacity = "1";
+// title[2].style.filter = "none";
+// title[2].style.transform = "none";
+// title[2].style.zIndex = "0";
+// title[2].style.position = "static";
+
+
+
