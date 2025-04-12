571d22b0b1f51a6a02ea35098fd047b7d29ed831
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-12 09:20:09 +0530

    updated

--- 25_Revision/16_dom/01_first.js
+++ 25_Revision/16_dom/01_first.js
@@ -1,6 +1,3 @@
-let title = document.getElementsByClassName("title");
-let container = document.getElementsByClassName("container");
-
 // selectors
 // document.getElementById("id");
 // document.getElementsByClassName("class");
@@ -16,50 +13,21 @@ let container = document.getElementsByClassName("container");
 // document.getElementsByClassName("title")[0].style.color = "red";
 
 
+let title = document.querySelectorAll(".title");
+let container = document.getElementsByClassName("container");
+let list_item1 = document.querySelectorAll(".mylist");
+let list_item2 = document.querySelectorAll(".mylist");
+
 title[2].textContent = "Hello World!";
 title[2].style.color = "red";
-// title[2].style.fontSize = "50px";
-// title[2].style.fontWeight = "bold";
-// title[2].style.textAlign = "center";
-// title[2].style.textDecoration = "underline";
-// title[2].style.textTransform = "uppercase";
-// title[2].style.backgroundColor = "black";
-// title[2].style.padding = "10px";
-// title[2].style.borderRadius = "10px";
-// title[2].style.boxShadow = "0 0 10px red";
-// title[2].style.margin = "10px";
-// title[2].style.width = "100%";
-// title[2].style.height = "100px";
-// title[2].style.display = "flex";
-// title[2].style.alignItems = "center";
-// title[2].style.justifyContent = "center";
-// title[2].style.fontFamily = "Arial, sans-serif";
-// title[2].style.fontStyle = "italic";
-// title[2].style.lineHeight = "100px";
-// title[2].style.letterSpacing = "5px";
-// title[2].style.textShadow = "2px 2px 5px black";
-title[2].style.transition = "all 0.5s ease-in-out";
-// title[2].style.cursor = "pointer";
-// title[2].style.opacity = "0.8";
-// title[2].style.filter = "blur(0px)";
-title[2].style.transform = "scale(1.1)";
-// title[2].style.zIndex = "1";
-// title[2].style.position = "relative";
-// title[2].style.top = "0px";
-// title[2].style.left = "0px";
-// title[2].style.right = "0px";
-// title[2].style.bottom = "0px";
-// title[2].style.border = "2px solid red";
-// title[2].style.boxSizing = "border-box";
-// title[2].style.overflow = "hidden";
-// title[2].style.whiteSpace = "nowrap";
-// title[2].style.textOverflow = "ellipsis";
-// title[2].style.visibility = "visible";
-// title[2].style.opacity = "1";
-// title[2].style.filter = "none";
-// title[2].style.transform = "none";
-// title[2].style.zIndex = "0";
-// title[2].style.position = "static";
 
 
+console.log(list_item1)
+list_item1.forEach((item) => {
+    // item.style.backgroundColor = "blue";
+    item.style.color = "green";
+    item.style.padding = "10px";
+    }
+);
 
+Array.from(list_item2).
