fed4dbc9c6e07445a751751cb77547b8f4affc4b
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-13 15:06:16 +0530

    updated

--- 25_Revision/16_dom/04_fourth.js
+++ 25_Revision/16_dom/04_fourth.js
@@ -1,9 +1,12 @@
 let childFirst = document.querySelector('#sub-container01');
-let childSecond = document.querySelector('#sub-container02');
+// let childSecond = document.querySelector('#sub-container02');
+let childSecond = childFirst.nextElementSibling;
 let parent = childFirst.parentElement;
 
-console.log(parent); // <div id="container">...</div>
+console.log(parent); //
+console.log(parent.childNodes); // Get all child elements of the parent element
 
 parent.style.backgroundColor = '#0f0f0f0f'; // Change the background color of the parent element
 childFirst.style.backgroundColor = 'green'; // Change the background color of the first child element
 childSecond.style.backgroundColor = 'green'; // Change the background color of the first child element
+
