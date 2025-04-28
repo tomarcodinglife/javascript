22f068845adc4f5d3457eb04e4ac6049d0b79238
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-28 20:04:23 +0530

    updated

--- 25_Revision/18_api/02_promise.js
+++ 25_Revision/18_api/02_promise.js
@@ -5,11 +5,26 @@ console.log("Hi i am js and today learning promise")
 const promiseOne = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Hi i am Promise First")
-    }, 1000)
+        resolve()
+    }, 2000)
 })
 
 // promice counsume 
-promiseOne.then()
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+})
 
 
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2")
+        resolve()
+    }, 2000)
+}).then(function(){
+    console.log("Async 2 resolved")
+})
 
+
+new Promise(function(resolve, reject){
+    
+})
