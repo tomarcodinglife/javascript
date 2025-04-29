20e938148e00871d7d5e32a434e0d2df13c0e1f5
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-29 08:35:56 +0530

    updated

--- 25_Revision/18_api/02_promise.js
+++ 25_Revision/18_api/02_promise.js
@@ -99,7 +99,7 @@ promiseFiveConsume()
 async function getAllUser() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = response.json()
+        const data = await response.json()
         console.log(data);
     } catch (e) {
         console.log("error :", e);
@@ -108,3 +108,13 @@ async function getAllUser() {
 
 getAllUser()
 
+// ---------------------------------------------------------------------
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data)
+}).catch((e) => {
+    console.log("error: ", e);
+})
