497332c334bbdfebb3a57e3961d99ca03e1bb2a8
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-30 19:06:50 +0530

    updated

--- 25_Revision/19_oops_and_classes/oop.js
+++ 25_Revision/19_oops_and_classes/oop.js
@@ -5,11 +5,32 @@ const user = {
 
     getUserDetails : function(){
         console.log(`username : ${this.username}, email : ${this.email}`);
+        console.log(this)
     }
 }
 
 console.log(user["logedIn"]);
 console.log(user.getUserDetails());
 
-const myPromise = new Promise()
-const myDate = new Date()
+// const myPromise = new Promise()
+// const myDate = new Date()
+
+function myUser (username, email, loginStatus){
+    this.username = username
+    this.email = email
+    this.loginStatus = loginStatus
+
+    return this // this is not required but if you write than code looking understable
+}
+
+let userFirst = new myUser("Sujit", "abc@gmail.com", true)
+let userSecond = new myUser("Tomar", "abc@gmail.com", false)
+
+console.log(userFirst);
+console.log(userSecond);
+
+
+
+
+
+
--- 25_Revision/19_oops_and_classes/theory.md
+++ 25_Revision/19_oops_and_classes/theory.md
@@ -12,5 +12,5 @@ Object Literal
 -Polymorphism
 
 ## Important Lines
-if you use new keyword than firstly you get new object and thats call instance after that call constructor and after that inject all your work after that your code properly work
+if you use new keyword than firstly you get new blank object and thats call instance -> call to constructor -> inject all your work after that your code properly work
 
