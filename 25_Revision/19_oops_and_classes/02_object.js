dd2fe5dd4ff4e2e9667206fcf65d820e17eeca5a
tomarcodinglife <tomarcodinglife@gmail.com>
2025-04-30 20:25:49 +0530

    updated

--- 25_Revision/19_oops_and_classes/02_object.js
+++ 25_Revision/19_oops_and_classes/02_object.js
@@ -1,9 +1,44 @@
+// 
 function squarValue (num){
     this.num = num * num
-    console.log(num) // 5
-    console.log(this.num) // 25
-    return this.num
+    // console.log(`${num}, ${this.num}`) 
+    return (`${num} and ${this.num}`)
 }
 
-squarValue(5)
+squarValue.squar = 2
+
+console.log(squarValue(5)); // 5 and 25
+console.log(squarValue.squar); // 2
+console.log(squarValue.prototype); // {}
+
+function newBook(BookName, BookPublication, BookAuthor, BookStock, BookPrice){
+    this.BookName = BookName
+    this.BookPrice = BookPrice
+    this.BookPublication = BookPublication
+    this.BookAuthor = BookAuthor
+    this.BookStock = BookStock
+}
+
+newBook.prototype.priceIncrease = function(){
+    increasePrice = Math.floor(this.BookPrice*10/100)
+    currentBookPrice = this.BookPrice + increasePrice
+    console.log(`Previous Price : ${this.BookPrice} and Current Book Price is ${currentBookPrice}`)
+}
+
+newBook.prototype.printBook = function(){
+    console.log(`Book Name : ${this.BookName}, Book Publication : ${this.BookPublication}, Book Author : ${this.BookAuthor}, Book in Stock: ${this.BookStock} and Book Price : ${this.BookPrice}`);   
+}
+
+let JSBook = new newBook ("AI With JavaScript", "Tomar Publication", "Sujit Tomar", 525, 999)
+let phpBook = new newBook ("myPHPWorld", "Tomar Publication", "Sujit Tomar", 25, 599)
+
+// console.log(JSBook);
+// console.log(phpBook);
+
+JSBook.printBook()
+JSBook.priceIncrease()
+
+
+
+
 
